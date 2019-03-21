48f1737d86268501f27f7cc4208cd1750192cb4a
Jaroslav Beno <jaroslav.beno@posam.sk>
2019-03-21 05:44:08 +0100

    Spring 30

--- src/main/java/sk/jaroslavbeno/springlearn2code/config/JarkoConfig.java
+++ src/main/java/sk/jaroslavbeno/springlearn2code/config/JarkoConfig.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import sk.jaroslavbeno.springlearn2code.services.impl.PrintService;
 
 @Configuration
-@PropertySource("classpath:jarko.properties")
+@PropertySources({
+        @PropertySource("classpath:jarko.properties"),
+        @PropertySource("classpath:beno.properties")
+})
 public class JarkoConfig {
 
     @Value("${jarko.meno}")
@@ -17,10 +21,13 @@ public class JarkoConfig {
     @Value("${jarko.priezvisko}")
     String priezvisko;
 
+    @Value("${jarko.beno.hocico}")
+    String hocico;
+
     @Bean
     public PrintService printService(){
         PrintService printService = new PrintService();
-        printService.setData(meno + ", "+ priezvisko);
+        printService.setData(meno + ", "+ priezvisko + "> "+hocico);
         return printService;
     }
 
