29607da9720a7bb58caa999323feb7340c96d70f
Jaroslav Beno <jaroslav.beno@posam.sk>
2019-04-04 09:04:56 +0200

    Spring 43

--- src/main/java/sk/jaroslavbeno/springlearn2code/repositories/MovieRepository.java
+++ src/main/java/sk/jaroslavbeno/springlearn2code/repositories/MovieRepository.java
@@ -1,8 +1,9 @@
 package sk.jaroslavbeno.springlearn2code.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import sk.jaroslavbeno.springlearn2code.model.Movie;
 
-public interface MovieRepository extends CrudRepository<Movie, Long> {
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 
 }
